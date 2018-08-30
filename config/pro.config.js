5f4ba0581aba42a5e36e396840385f7748715a4d
skygst102 <skygst@sina.cn>
2018-08-30 23:41:50 +0800

    ''

--- config/pro.config.js
+++ config/pro.config.js
@@ -6,11 +6,7 @@ const devServer = require("./devServer");
 module.exports = {
     mode: 'production',
     entry: entry,
-    output: {
-        path: path.resolve("./dist"), //输出路径，要用绝对路径
-        filename: "js/[name]/[name].[chunkhash:8].js",
-        publicPath: "" //html中资源路径想对路径
-    },
+    output: output,
     module: loader,
     plugins: plugins,
 };
