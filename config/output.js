c9d0ea5589043f0ae155690987c32b68f8549f31
skygst102 <skygst@sina.cn>
2018-09-01 22:28:03 +0800

    ''

--- config/output.js
+++ config/output.js
@@ -2,5 +2,5 @@ const path = require("path");
 module.exports={
     path:path.resolve(__dirname,'../dist'), //输出路径，要用绝对路径
     filename: "assets/js/[name]/[name].[chunkhash:8].js",
-    publicPath: "../dist" //html中资源路径想对路径
+    publicPath: "../dist/" //html中资源路径想对路径
 }
